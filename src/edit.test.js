3750862fa46f21bfa31c227eb72f0076a3f14a17
Md.Abdullah al mamun <mamun1214@gmail.com>
2023-03-10 15:18:16 +0600

    Update

--- src/edit.test.js
+++ src/edit.test.js
@@ -7,8 +7,8 @@
 import Apps from './modules/Apps';
 import Todo from './modules/Todo';
 
-const apps = new Apps();
 describe('Edit function', () => {
+  const apps = new Apps();
   const desc = 'This is changed item';
   const $item = new Todo('Jest Test', false, 1);
   apps.saveItems($item);
